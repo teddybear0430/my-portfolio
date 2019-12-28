2d6e9c8592f61d0c4420d340bd19f8bddd764e79
teddybear0430 <teddybear0430@hotmail.com>
2019-12-28 16:39:33 +0900

    ブラウザ上で実行されていないときはnullを返すように修正。(SSR対策)

--- app/src/components/Header/Animation.js
+++ app/src/components/Header/Animation.js
@@ -3,15 +3,18 @@ import ParticlesBg from 'particles-bg';
 import '../../styles/header.scss';
 
 const Animation = () => {
-    return (
-        <div id="Animation">
-            <ParticlesBg color="#bb99ff" type="circle" bg={true}></ParticlesBg>
-            <div className="title-container">
-                <h1 className="title">My Portfolio</h1>
-                <span className="sub-title">Web Developer</span>
+    if (typeof window !== 'undefined') {
+        return (
+            <div id="Animation">
+                <ParticlesBg color="#bb99ff" type="circle" bg={true}></ParticlesBg>
+                <div className="title-container">
+                    <h1 className="title">My Portfolio</h1>
+                    <span className="sub-title">Web Developer</span>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    return null
 }
 
 export default Animation;
