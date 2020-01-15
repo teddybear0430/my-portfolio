ad5977baecb4db1ab81792e292d29fa29ca328c6
teddybear0430 <teddybear0430@hotmail.com>
2020-01-15 23:23:39 +0900

    フッター下部にgithubアイコンを表示するようにした。

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from '@material-ui/core';
 import '../../styles/footer.scss';
 
 const nowYear = () => {
@@ -10,6 +13,11 @@ const Footer = ({ title }) => {
     return (
         <footer id="Footer">
             <span className="copyright">Copyright {title} © {nowYear()}</span>
+            <div className="icon-container">
+                <Link href="https://github.com/Yota-K" color="inherit">
+                    <FontAwesomeIcon className="github-icon" icon={faGithub} />
+                </Link>
+            </div>
         </footer>
     );
 }
