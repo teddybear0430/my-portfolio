cae6c0878dfcba14b13e9dec6c2129c1d187800c
teddybear0430 <teddybear0430@hotmail.com>
2020-01-15 23:53:30 +0900

    アイコンをLinkではなく、aタグで囲むように修正。

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Link } from '@material-ui/core';
 import '../../styles/footer.scss';
 
@@ -13,6 +13,11 @@ const Footer = ({ title }) => {
     return (
         <footer id="Footer">
             <span className="copyright">Copyright {title} © {nowYear()}</span>
+            <div className="icon-container">
+                <a href="https://github.com/Yota-K" color="inherit">
+                    <FontAwesomeIcon className="github-icon" icon={faGithub} />
+                </a>
+            </div>
         </footer>
     );
 }
