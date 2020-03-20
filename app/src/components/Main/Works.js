a6538b945fe21cffd4ba929e9d0ee2f5f311ae08
teddybear0430 <teddybear0430@hotmail.com>
2020-03-20 15:51:41 +0900

    タグの構造を変更。(スタイルも追加。)

--- app/src/components/Main/Works.js
+++ app/src/components/Main/Works.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Img from 'gatsby-image';
+
 import '../../styles/works.scss';
 
 const Works = ({ 
@@ -12,38 +13,59 @@ const Works = ({
         image3,
         image4,
     }) => {
+
+    const contentObj = [
+        {
+            title: 'My Portfolio',
+            link: 'https://amazing-wozniak-9cea93.netlify.com/',
+            thumbnail: image1,
+            overview: works1,
+            technology: 'React/GatsbyJS/GraphQL',
+        },
+        {
+            title: 'Flex Generator',
+            link: 'https://yota-k.github.io/flex-generator/',
+            thumbnail: image2,
+            overview: works2,
+            technology: 'React/Material-UI/styled-components/',
+        },
+        {
+            title: 'ペンギンメディア',
+            link: '',
+            thumbnail: image3,
+            overview: works3,
+            technology: 'PHP/WordPress/JavaScript/Webpack',
+        },
+        {
+            title: 'ポケモン育成論投稿サイト',
+            link: 'http://pokemon-app-sample.herokuapp.com/',
+            thumbnail: image4,
+            overview: works4,
+            technology: 'Laravel/JavaScript/bootstrap/heroku',
+        }
+    ];
+
     return (
         <section id="Works">
             <h2 className="heading">Works</h2>
             <div className="portfolios">
-                <div className="portfolio">
-                    <h3><a href="https://amazing-wozniak-9cea93.netlify.com/">My Portfolio</a></h3>
-                    <Img fluid={image1} />
-                    <div className="portfolio-content" dangerouslySetInnerHTML={{ __html: works1 }} />
-                    <h4>使用した主な技術</h4>
-                    React/GatsbyJS/GraphQL
-                </div>
-                <div className="portfolio">
-                    <h3><a href="https://yota-k.github.io/energy_drink/">エナドリ</a></h3>
-                    <Img fluid={image2} />
-                    <div  className="portfolio-content" dangerouslySetInnerHTML={{ __html: works2 }} />
-                    <h4>使用した主な技術</h4>
-                    React/Gulp/bootstrap
-                </div>
-                <div className="portfolio">
-                    <h3>ペンギンメディア</h3>
-                    <Img fluid={image3} />
-                    <div className="portfolio-content" dangerouslySetInnerHTML={{ __html: works3 }} />
-                    <h4>使用した主な技術</h4>
-                    PHP/WordPress/JavaScript/Webpack
-                </div>
-                <div className="portfolio">
-                    <h3><a href="http://pokemon-app-sample.herokuapp.com/">ポケモン育成論投稿サイト</a></h3>
-                    <Img fluid={image4} />
-                    <div className="portfolio-content" dangerouslySetInnerHTML={{ __html: works4 }} />
-                    <h4>使用した主な技術</h4>
-                    Laravel/JavaScript/bootstrap/heroku
-                </div>
+                {contentObj.map((content, i) => (
+                        <div className="portfolio" key={i}>
+                            <div className="portfolio-container">
+                                <h3>{
+                                    content.link !== ''
+                                    ? <a href={content.link}>{content.title}</a>
+                                    : <>{content.title}</>
+                                }</h3>
+                                <Img fluid={content.thumbnail} />
+                                <div className="portfolio-overview" dangerouslySetInnerHTML={{ __html: content.overview }} />
+                                <div className="portfolio-technology">
+                                    <h4>使用した主な技術</h4>
+                                    <p>{content.technology}</p>
+                                </div>
+                            </div>
+                        </div>
+                ))}
             </div>
         </section>
     );
